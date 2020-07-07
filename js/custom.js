a0601877e306349ad347216be69f62fb2dd036b1
gagseo <gagseo@gmail.com>
2020-07-07 15:44:41 +0900

    20200707
    js에 modal추가

--- js/custom.js
+++ js/custom.js
@@ -106,6 +106,9 @@ $(document).ready(function () {
     //animatedModal
     $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
 
+    //animatedModal1
+    $("#demo10").animatedModal1();
+
     // Contact Form 	
 
     // validate contact form
